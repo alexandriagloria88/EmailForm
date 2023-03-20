7649c27fdfa91bd4ededb68930354ad0bc8a76cd
Dpuk123 <padipu9@gmail.com>
2023-03-20 10:36:22 +0700

    Initial commit

---
+++ app/src/main/java/com/alexandriagloria_202102326/emailform/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alexandriagloria_202102326.emailform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
